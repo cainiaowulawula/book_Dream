6400dff58347f8d94a75f6f5130688b6c3f6ba59
gaoqiquan <g2994936517@163.com>
2019-06-10 18:01:39 +0800

    更新

--- book_dream/src/components/details/details.vue
+++ book_dream/src/components/details/details.vue
@@ -6,6 +6,10 @@
       <van-swipe-item>3</van-swipe-item>
       <van-swipe-item>4</van-swipe-item>
     </van-swipe>
+    <van-row>
+      <van-col span="9">span: 8</van-col>
+      <van-col span="15">span: 8</van-col>
+    </van-row>
   </div>
 </template>
 <script>
@@ -18,17 +22,22 @@ export default {
 };
 </script>
 <style>
-.van-swipe{
-    background-color: #66c6f2;
-    height: 150px;
-    text-align: center;
-    color: white
+.van-swipe {
+  background-color: #66c6f2;
+  height: 150px;
+  text-align: center;
+  color: white;
 }
-.van-swipe-item{
-    margin-top: 65px 
+.van-swipe-item {
+  margin-top: 65px;
 }
-body{
-    background-color: #f2f3f5
+body {
+  background-color: #f2f3f5;
+}
+.van-col{
+    border: 1px solid #66c6f2;
+    background-color: aquamarine;
+    height: 50px;
 }
 </style>
 
